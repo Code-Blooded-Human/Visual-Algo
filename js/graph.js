12ad234d592d2dc2606681386496e889f5ded6ab
Abhishek <abhisheks@iitbhilai.ac.in>
2019-11-23 21:25:03 +0530

    added graph.getGraph() returns graph data in json format for cytoscape plotting function

--- js/graph.js
+++ js/graph.js
@@ -2,9 +2,13 @@ class vertex{
   id;
   name;
   children;
-  constructor(id,name){
+  x;
+  y;
+  constructor(id,name,x,y){
     this.id=id;
     this.name=name;
+    this.x=x;
+    this.y=y;
     this.children= new Array();
   }
   addChildren(id){
@@ -32,8 +36,8 @@ class graph{
     this.edges=new Array();
   }
 
-  addVertex(name){
-    this.vertices.push(new vertex(this.vertices.length,name));
+  addVertex(name,x,y){
+    this.vertices.push(new vertex(this.vertices.length,name,x,y));
   }
 
   addEdge(to,from,weight=1){
@@ -44,4 +48,38 @@ class graph{
       this.vertices[to].addChildren(from);
     }
   }
+
+  getGraph(){
+    var graphData=new Array();
+    for(var i=0;i<this.vertices.length;i++){
+      var node  = {};
+      node.group='nodes';
+      var data={};
+      data.id=i;
+      var position ={};
+      position.x=this.vertices[i].x;
+      position.y=this.vertices[i].y;
+      node.data=data;
+      node.position=position;
+      graphData.push(node);
+    }
+    for(var i=0;i<this.edges.length;i++){
+      var edge ={};
+      edge.group="edges";
+      var data={};
+      data.id="e"+i;
+      data.source=this.edges[i].from;
+      data.target=this.edges[i].to;
+      edge.data=data;
+      graphData.push(edge);
+    }
+    return graphData;
+  }
 }
+g= new graph("ud");
+g.addVertex("A",100,100);
+g.addVertex("B",200,100);
+g.addVertex("C",100,200);
+g.addVertex("D",200,200);
+g.addEdge(1,0,1);
+g.addEdge(3,1,1);
