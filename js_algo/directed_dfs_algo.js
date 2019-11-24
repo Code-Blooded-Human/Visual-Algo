0b2fc5a4206e87919896e9fdace50a3a797082d1
Abhishek <abhisheks@iitbhilai.ac.in>
2019-11-24 12:02:09 +0530

    directed dfs with default data

---
+++ js_algo/directed_dfs_algo.js
@@ -0,0 +1,57 @@
+
+var dfsStack = new stack();
+
+function areAllVerticesVisited(children){
+  for(i=0;i<children.length;i++){
+    if(g.vertices[children[i]].visited==false){
+      return false;
+    }
+}
+return true;
+
+}
+
+function dfs(node){
+  g.vertices[node].visited=true;
+  dfsStack.push(node);
+  cy.getElementById(node).addClass('visited');
+}
+
+
+ function dfs_next(){
+
+  if(dfsStack.isEmpty()){
+    return true;
+  }else{
+  var n = dfsStack.peek();
+  if(g.vertices[n].children.length==0 || areAllVerticesVisited(g.vertices[n].children)){
+    dfsStack.pop();
+    cy.getElementById(n).addClass('done');
+    console.log("POP-->"+n);
+  }else{
+
+      for(var i=0; i< g.vertices[n].children.length;i++){
+
+        if(!g.vertices[g.vertices[n].children[i]].visited){
+
+          dfsStack.push(g.vertices[n].children[i]);
+          g.vertices[g.vertices[n].children[i]].visited=true;
+          cy.getElementById(g.vertices[n].children[i]).addClass('visited');
+          cy.getElementById("e"+""+n+""+g.vertices[n].children[i]).addClass("edgeVisited");
+          break;
+        }
+    }
+  }
+return false;
+}
+}
+
+var start = function(){
+  if(dfs_next()==true){
+
+  }else{
+    setTimeout(start,1000);
+  }
+}
+
+dfs(0);
